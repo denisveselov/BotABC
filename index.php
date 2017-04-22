4dd3b65f122203050e4e37207bf2fef907f3c6f5
denisveselov <vorle57@gmail.com>
2017-04-22 22:03:49 +0300

    v. 0.0.4
    Reformat Code

--- index.php
+++ index.php
@@ -5,13 +5,14 @@ $message = $output['message']['text'];
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
 
 switch ($message){
-    case 'Привет';
+
+    case 'Привет':
         $message('Добрый день! Меня зовут АктивМэн');
         sendMessage($tokken, $id, $message);
         break;
 
 
-    case 'Как дела?';
+    case 'Как дела?':
         $message('Дела гуд, но на линию не выпускают');
         sendMessage($tokken, $id, $message);
         break;
@@ -20,10 +21,9 @@ switch ($message){
     default:
         $message('Пиши биз ашибак!');
         sendMessage($tokken, $id, $message);
-        
 }
 function sendMessage($tokken, $id, $message)
 {
-    file_get_contents("https://api.telegram.org/bot".$tokken."/sendMessage?chat_id=".$id."&text=".$message);
+    file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $message);
 }
 file_put_contents("logs.txt", $output);

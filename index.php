87db8478c9c218b420078fb1928f3b3608baf123
denisveselov <vorle57@gmail.com>
2017-04-22 21:00:54 +0300

    v. 0.0.3

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 $output = json_decode(file_get_contents('php://input'),true);
+$id = $output['message']['chat']['id'];
+file_get_contents("https://api.telegram.org/bot339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU/sendMessage?chat_id = ".$id."&text = Шо тобi треба? ");
 file_put_contents("logs.txt", $output);

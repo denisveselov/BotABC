63980a4db4e742d3380073887ac93ae486b612d6
denisveselov <vorle57@gmail.com>
2017-04-22 21:31:38 +0300

    v. 0.0.4
    Reformat Code

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
-
+sendMessage($tokken, $id);
 function sendMessage($tokken, $id)
 {
     file_get_contents("https://api.telegram.org/bot".$tokken."/sendMessage?chat_id=".$id."&text=Пока что я глупый Бот");

551d18b5f6cd35572b4ff58ef161d04d4b5de9a4
denisveselov <vorle57@gmail.com>
2017-04-22 20:38:56 +0300

    v. 0.0.2

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-$output = file_get_contents('php://input');
-file_put_contents('logs.txt', $output);
+$output = json_decode(file_get_contents('php://input'),true);
+file_put_contents("logs.txt", $output);

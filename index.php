718fdbdb65762b81d95b32171a217a962fe0e28f
denisveselov <vorle57@gmail.com>
2017-04-22 21:57:39 +0300

    v. 0.0.4
    Reformat Code

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
+$message = $output['message']['text'];
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
-sendMessage($tokken, $id);
-function sendMessage($tokken, $id)
+
+switch ($message){
+    case 'Привет';
+        $message('Добрый день! Меня зовут АктивМэн');
+        sendMessage($tokken, $id, $message);
+        break;
+
+
+    case 'Как дела?';
+        $message('Дела гуд, но на линию не выпускают');
+        sendMessage($tokken, $id, $message);
+        break;
+
+
+    default:
+        $message('Пиши биз ашибак!');
+        sendMessage($tokken, $id, $message);
+        
+}
+function sendMessage($tokken, $id, $message)
 {
-    file_get_contents("https://api.telegram.org/bot".$tokken."/sendMessage?chat_id=".$id."&text=Пока что я глупый Бот");
+    file_get_contents("https://api.telegram.org/bot".$tokken."/sendMessage?chat_id=".$id."&text=".$message);
 }
 file_put_contents("logs.txt", $output);

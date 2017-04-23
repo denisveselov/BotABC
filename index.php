9833d0dce06d8261ad0fe7cf6362254629c2cf5d
denisveselov <vorle57@gmail.com>
2017-04-23 11:27:21 +0300

    v. 0.0.5
     test ifelse

--- index.php
+++ index.php
@@ -8,65 +8,11 @@ if(in_array($message,$case)){
     $message = 'Привет! Меня зовут АктивМэн';
     sendMessage($tokken, $id, $message);
 }
-else $message = 'Пеши биз ашыбак';
-     sendMessage($tokken, $id, $message);
-
-/*
-switch ($message){
-
-    case 'Привет', '';
-    $message = 'Добрый день! Меня зовут АктивМэн';
-        sendMessage($tokken, $id, $message);
-        break;
-
-
-    case 'Как дела?':
-        $message = 'Дела гуд, но на линию не выпускают';
-        sendMessage($tokken, $id, $message);
-        break;
-
-
-    case 'Почему?':
-        $message = 'Нет работы с голосом! МВ она такая!';
-        sendMessage($tokken, $id, $message);
-        break;
-
-    case 'ахаха':
-        $message = 'Не смешно! Я на 3 скилл хочу!';
-        sendMessage($tokken, $id, $message);
-        break;
-
-    case 'Где мы находимся?':
-        $message = 'В Орле: Ломоносова д. 6 БЦ "Модус" В Москве: ул. Дмитрия Ульянова 7А БЦ "МАЙ"';
-        sendMessage($tokken, $id, $message);
-        break;
-
-    case 'Должник умер':
-        $message = 'Запроси максимум инфо: дату смерти, копию свидетельства на эл.почту, Результат: "Подозрение на исключение"';
-        sendMessage($tokken, $id, $message);
-        break;
-
-    case 'Какой порог на этот месяц':
-        $message = 'Для линии Сбербанк: 540000руб/68000руб/810000руб. Если ты работаешь 4 часа - то дели на два. Если работаешь 6 часов умножай на 0,75';
-        sendMessage($tokken, $id, $message);
-        break;
-
-    case 'С какого раза удаляем номер для проверки':
-        $message = 'Если из реестра, то со второго, если добавлен оператором, то с первого';
-        sendMessage($tokken, $id, $message);
-        break;
-
-    case 'Когда зарплата':
-        $message = 'Каждого 20го и 5го числа месяца';
-        sendMessage($tokken, $id, $message);
-        break;
-
-
-    default:
-        $message = 'Пиши биз ашибак!';
-        sendMessage($tokken, $id, $message);
+else {
+    $message = 'Пеши биз ашыбак';
+    sendMessage($tokken, $id, $message);
 }
-*/
+
 function sendMessage($tokken, $id, $message)
 {
     file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $message);

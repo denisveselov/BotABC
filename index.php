a02a5ecccd6b9b4c225ad6a46f50339f39cf4151
denisveselov <vorle57@gmail.com>
2017-04-23 11:14:52 +0300

    v. 0.0.5
     test if

--- index.php
+++ index.php
@@ -3,16 +3,17 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
+$case = array('Привет','Привет!','Дратути','привет','Здорова!','здорово','Хай', );
+if(in_array($message,$case)){
+    $message = 'Привет! Меня зовут АктивМэн';
+    sendMessage($tokken, $id, $message);
+}
 
 
+/*
 switch ($message){
 
-    case 'Привет';
-    case 'Привет!';
-    case 'Дарова';
-    case 'Здравствуйте';
-    case 'Хай';
-    case 'Дратути';
+    case 'Привет', '';
     $message = 'Добрый день! Меня зовут АктивМэн';
         sendMessage($tokken, $id, $message);
         break;
@@ -64,6 +65,7 @@ switch ($message){
         $message = 'Пиши биз ашибак!';
         sendMessage($tokken, $id, $message);
 }
+*/
 function sendMessage($tokken, $id, $message)
 {
     file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $message);

d1c4e76f7feee50d45afb8c9d66cb5735098d77a
denisveselov <vorle57@gmail.com>
2017-04-23 11:22:09 +0300

    v. 0.0.5
     test if

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
-$case = array('Привет','Привет!','Дратути','привет','Здорова!','здорово','Хай', );
+$case = array('Привет','Привет!','Дратути','привет','Здорова!','здорово','Хай', 'добрый день','Добрый день!','привет!','Приветствую!','приветствую!','Здравствуй','Здравствуй!',);
 if(in_array($message,$case)){
     $message = 'Привет! Меня зовут АктивМэн';
     sendMessage($tokken, $id, $message);
 }
-
+else $message = 'Пеши биз ашыбак';
+     sendMessage($tokken, $id, $message);
 
 /*
 switch ($message){

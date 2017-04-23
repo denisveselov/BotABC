6eeb37c7f775daf0e3f71e6e0a477eadf4b98516
denisveselov <vorle57@gmail.com>
2017-04-24 02:11:44 +0300

    v. 0.0.9.3

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
+$url = 'https://api.telegram.org/bot'.$tokken. '/'; //переменная линк токкена
+
+
 include 'cases.php';
+
+
 while ($message){
     if (in_array($message, $hello_case)) {
         $message = 'Привет! Меня зовут АктивМэн';
@@ -24,9 +29,9 @@ while ($message) {
     }
     break;
 }
-function sendMessage($tokken, $id, $message)
+function sendMessage($tokken, $id, $message, $url)
 {
-    file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $message);
+    file_get_contents($url. $tokken ."sendMessage?chat_id=". $id ."&text=". $message);
 }
 file_put_contents("logs.txt", $output);
 
@@ -47,7 +52,6 @@ $chat_id = $output['message']['chat']['id']; //запрос последнего
 echo $chat_id;
 
 $tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
-$url = 'https://api.telegram.org/bot'.$tokken. '/'; //переменная линк токкена
 $users_message = $output['message']['text']; //запрос текста последнего сообщения
 
 include 'cases.php';

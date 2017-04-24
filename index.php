1c76668473cc443abe24b166a0335d2346c9aeb1
denisveselov <vorle57@gmail.com>
2017-04-24 12:13:44 +0300

    v. 1.0.0.
    alfa version

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
 <?php
-$tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
-$url = 'https://api.telegram.org/bot'; //переменная линк токкена
-
-
-
-$output = json_decode(file_get_contents('php://input')); //получаем массив
+$output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
-
-
+$tokken = '339498031:AAGS0gW6vqOjY9hiN8bAT7A7S1qPI-ZWUCU';
 include 'cases.php';
-
-if (in_array($message, $hello_case)) {
+if(in_array($message,$hello_case)){
     $message = 'Привет! Меня зовут АктивМэн';
     sendMessage($tokken, $id, $message);
-} else {
-    $message = 'Я тебя не совсем понял';
+}
+if(in_array($message,$bye_case)){
+    $message = 'До побачення! Ой! До связи. А ты завтра работаешь?)';
+    sendMessage($tokken, $id, $message);{
+        if ($message == 'Да'){
+            $message = 'Тогда завтра спишемся)';
+            sendMessage($tokken, $id, $message);
+        }
+        else{
+            $message = 'Я буду скучать(';
+            sendMessage($tokken, $id, $message);
+        }
+    }
+}
+else {
+    $message = 'Пеши биз ашыбак';
     sendMessage($tokken, $id, $message);
 }
-
-
 function sendMessage($tokken, $id, $message)
 {
     file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $message);

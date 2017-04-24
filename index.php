65a62b00c9677ba13e61a7f8180e17b8abdf4f57
denisveselov <vorle57@gmail.com>
2017-04-24 12:06:31 +0300

    v. 0.0.9.3
    URL

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ $message = $output['message']['text'];
 include 'cases.php';
 
 
-while ($message){
+while ($message) {
     if (in_array($message, $hello_case)) {
         $message = 'Привет! Меня зовут АктивМэн';
         sendMessage($tokken, $id, $message);
@@ -20,24 +20,12 @@ while ($message){
         $message = 'Я тебя не совсем понял';
         sendMessage($tokken, $id, $message);
     }
-    break;
-}
-while ($message) {
-    if (in_array($message, $bye_case)) {
-        $message = 'Пока! Ой! До побачення)';
-        sendMessage($tokken, $id, $message);
-    } else {
-        $message = 'Пиши без ошибок';
-        sendMessage($tokken, $id, $message);
-    }
-    break;
 }
 function sendMessage($tokken, $id, $message)
 {
     file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $message);
 }
 file_put_contents("logs.txt", $output);
-
 
 
 

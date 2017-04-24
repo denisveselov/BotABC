e59d3e49315b3962d700f4d08bc8f9c82c3ef933
denisveselov <vorle57@gmail.com>
2017-04-24 19:21:32 +0300

    v. 1.0.6
    mb_strtolower

--- global_var.php
+++ global_var.php
@@ -1,8 +1,9 @@
 <?php
 $update = file_get_contents('php://input');
 $update = json_decode($update, true);
+
 $chat_id = $update['message']['chat']['id'];
 $user_name = $update['message']['from']['username'];
-$message = $update['message']['text'];
+$message = mb_strtolower($update['message']['text'],'UTF-8');
 $message_id = $update['message']['message_id'];
 $message_name = $update['message']['chat']['first_name'];

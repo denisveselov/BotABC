883ed255c26bca467cc66019d6ca1fb56dd04122
denisveselov <vorle57@gmail.com>
2017-04-24 16:56:59 +0300

    v. 1.0.6
    test refactor

--- global_var.php
+++ global_var.php
@@ -1,4 +1,8 @@
 <?php
-$output = json_decode(file_get_contents('php://input'), true);
-$id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
+$update = file_get_contents('php://input');
+$update = json_decode($update, true);
+$chat_id = $update['message']['chat']['id'];
+$user_name = $update['message']['from']['username'];
+$message = $update['message']['text'];
+$message_id = $update['message']['message_id'];
+$message_name = $update['message']['chat']['first_name'];
--- index.php
+++ index.php
@@ -19,18 +19,18 @@ function responses ()
 while ($message) {
     if (in_array($message, $hello_case)) {
         $ans_message = $hello_answer[mt_rand(0, count($hello_answer) - 1)];
-        sendMessage($tokken, $id, $ans_message);
+        sendMessage($tokken, $chat_id, $ans_message);
     } else {
         $ans_message = $error_answer;
-        sendMessage($tokken, $id, $ans_message);
+        sendMessage($tokken, $chat_id, $ans_message);
     }
     break;
 }
 
 //send Messages end put to logs file
-function sendMessage($tokken, $id, $ans_message)
+function sendMessage($tokken, $chat_id, $ans_message)
 {
-    file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $id ."&text=". $ans_message);
+    file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $chat_id ."&text=". $ans_message);
 }
-file_put_contents("logs.txt", $output);
+file_put_contents("logs.txt", $update);
 

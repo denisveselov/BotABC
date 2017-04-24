cab6bca3eba6dbc8fe0aed875b2feb455d05b86d
denisveselov <vorle57@gmail.com>
2017-04-24 15:36:45 +0300

    v. 1.0.5
    test last up id

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 $output = json_decode(file_get_contents('php://input'), true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
+return $message;
 
 /*
 function responses ()

93419c054083a074cbfb2a5e8ba9e3af97450848
denisveselov <vorle57@gmail.com>
2017-04-25 04:40:34 +0300

    v. 1.1.1
    добавлены анекдоты
    
    v. 1.1.2
    
    - case what's up

--- index.php
+++ index.php
@@ -6,14 +6,7 @@ include 'tokken_var.php';
 include 'cases.php';
 include 'aswer.php';
 
-/*
-function responses ()
-{
-    global $output;
-    global $id;
-    global $message;
-}
-*/
+
 
 switch (in_array($message,$hello_case)){
     case TRUE:
@@ -57,49 +50,16 @@ switch (in_array($message,$jokes_case)) {
         sendMessage($tokken, $chat_id, $ans_message);
         break;
 }
-
-
-
-/*
-switch (in_array($message,$bye_case)) {
+switch (in_array($message,$whatsup_case)) {
     case TRUE:
-    case 'Почему?':
-        $ans_message = 'Нет работы с голосом! МВ она такая!';
+        $ans_message = $whatsup_answer[mt_rand(0, count($whatsup_answer) - 1)];
         sendMessage($tokken, $chat_id, $ans_message);
         break;
-    case 'ахаха':
-        $ans_message = 'Не смешно! Я на 3 скилл хочу!';
-        sendMessage($tokken, $chat_id, $ans_message);
-        break;
-    case 'Где я сейчас?':
-        $ans_message = 'Я не Яндекс-Карты - оглянись вокруг блин!';
-        sendMessage($tokken, $chat_id, $ans_message);
-        break;
-    case 'В чем смысл жизни?':
-        $ans_message = 'У каждого свой, твой зарабатывать деньги!';
-        sendMessage($tokken, $chat_id, $ans_message);
-        break;
-
-    default:
-        $ans_message = 'Пиши биз ашибак!';
-        sendMessage($tokken, $chat_id, $ans_message);
 }
-*/
 
 
-/*
-// Logic //
-while ($message) {
-    if (in_array($message, $hello_case)) {
-        $ans_message = $hello_answer[mt_rand(0, count($hello_answer) - 1)];
-        sendMessage($tokken, $chat_id, $ans_message);
-    } else {
-        $ans_message = $error_answer;
-        sendMessage($tokken, $chat_id, $ans_message);
-    }
-    break;
-}
-*/
+
+
 
 //send Messages end put to logs file
 function sendMessage($tokken, $chat_id, $ans_message)

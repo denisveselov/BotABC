e9ea160ab2aa703adfaa0c95b5fc2f7fac6806dc
denisveselov <vorle57@gmail.com>
2017-04-28 02:40:16 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- db.php
+++ db.php
@@ -8,13 +8,6 @@ R::setup( 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_6ae1fc935ee
 session_start();
 /* var_dump(R::testConnection());*/ // проверка подлючения
 
-//create array $update in DB
-$arr_messages = R::dispense('arraymessages');
-$arr_messages->user_name = $user_name;
-$arr_messages->chat_id = $chat_id;
-$arr_messages->message_id = $message_id;
-$arr_messages->message_txt = $message;
-$id = R::store($arr_messages);
 ?>
 
 

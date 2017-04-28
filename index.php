aaeb41bf0f4bb1c097a0984f7269a566561c179b
denisveselov <vorle57@gmail.com>
2017-04-29 00:42:09 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- index.php
+++ index.php
@@ -16,7 +16,6 @@ include 'aswer.php';
 $usermessages1 = R::dispense('usermessages1');
 $usermessages1->chat_id = $chat_id;
 $usermessages1->message_id = $message_id;
-$usermessages1->user_name = $user_name;
 
 $id = R::store($usermessages1);
 

f61f3edf5e3775a82764c00b385fa74aa934f547
denisveselov <vorle57@gmail.com>
2017-04-30 00:27:42 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- global_var.php
+++ global_var.php
@@ -5,6 +5,7 @@ $chat_id = $update['message']['chat']['id'];
 $user_name = $update['message']['from']['username'];
 $message = $update['message']['text'];
 $txt_msg = $message;
+$txt_msg=iconv("utf-8", "cp1251", $txt_msg['text']);
 $message_id = $update['message']['message_id'];
 $message_name = $update['message']['chat']['first_name'];
 
@@ -14,5 +15,5 @@ $message_name = $update['message']['chat']['first_name'];
 $user_messages = R::dispense('usermessages');
 $user_messages->chat_id = $chat_id;
 $user_messages->message_id = $message_id;
-/*$user_messages->message = $txt_msg;*/
+$user_messages->message_txt = $txt_msg;
 $id = R::store($user_messages);

83116962947c11bf629aa380efb13f36ec313ff5
denisveselov <vorle57@gmail.com>
2017-04-29 23:29:39 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- global_var.php
+++ global_var.php
@@ -5,6 +5,7 @@ var_dump($update);
 $chat_id = $update['message']['chat']['id'];
 $user_name = $update['message']['from']['username'];
 $message = $update['message']['text'];
+$txt_msg = $message;
 $message_id = $update['message']['message_id'];
 $message_name = $update['message']['chat']['first_name'];
 

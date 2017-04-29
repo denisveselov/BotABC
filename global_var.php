330b0c9d4b67fc7807a3cd1264986da0ea94d0f5
denisveselov <vorle57@gmail.com>
2017-04-29 23:20:44 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- global_var.php
+++ global_var.php
@@ -1,11 +1,11 @@
 <?php
 $update = file_get_contents('php://input');
 $update = json_decode($update, true);
-$chat_id = $update['result']['message']['chat']['id'];
-$user_name = $update['result']['message']['from']['username'];
-$message = $update['result']['message']['text'];
-$message_id = $update['result']['message']['message_id'];
-$message_name = $update['result']['message']['chat']['first_name'];
+$chat_id = $update['message']['message']['chat']['id'];
+$user_name = $update['message']['from']['username'];
+$message = $update['message']['text'];
+$message_id = $update['message']['message_id'];
+$message_name = $update['message']['chat']['first_name'];
 
 
 //CREATE to DB RedBeanPHP

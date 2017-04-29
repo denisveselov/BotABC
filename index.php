ce465ba3b709d8a7902e4a3586182b9c594a4d85
denisveselov <vorle57@gmail.com>
2017-04-29 23:58:57 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- index.php
+++ index.php
@@ -95,7 +95,7 @@ function sendMessage($tokken, $chat_id, $ans_message)
 {
     file_get_contents("https://api.telegram.org/bot". $tokken ."/sendMessage?chat_id=". $chat_id ."&text=". $ans_message);
 }
-file_put_contents("logs.txt", $update);
+/*file_put_contents("logs.txt", $update);*/
 
 
 

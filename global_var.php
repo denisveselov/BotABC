06c7f97294466a0fa7d5c7bc6451ec6e57db362d
denisveselov <vorle57@gmail.com>
2017-04-30 15:32:09 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- global_var.php
+++ global_var.php
@@ -5,7 +5,6 @@ $chat_id = $update['message']['chat']['id'];
 $user_name = $update['message']['from']['username'];
 $message = $update['message']['text'];
 $messageDB = $message;
-$message = mb_convert_encoding($message, "cp1251", "utf-8");
 /*
 $message = iconv('UTF-8', 'CP1251', $message);
 $message = iconv('CP1251', 'UTF-8', $message);
@@ -14,5 +13,15 @@ $message_id = $update['message']['message_id'];
 $message_name = $update['message']['chat']['first_name'];
 
 
-
-
+/*
+//Remove UTF8 Bom
+function remove_utf8_bom($txt_msq){
+    $bom = pack('H*','EFBBBF');
+    $txt_msq = preg_replace("/^$bom/", '', $txt_msq);
+    return $txt_msq;
+}
+*/
+$user_messages = R::dispense('usermessages');
+$user_messages->chat_id = $chat_id;
+$user_messages->message_id = $message_id;
+$id = R::store($user_messages);

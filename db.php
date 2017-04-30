c7026c186f1cf3eb82b72265b2fe3b36a92d1283
denisveselov <vorle57@gmail.com>
2017-04-30 14:12:43 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- db.php
+++ db.php
@@ -6,9 +6,20 @@ R::setup( 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_6ae1fc935ee
     'b63a3b9e8e665e', '4f6ef433' );
 
 session_start();
-/* var_dump(R::testConnection());*/ // проверка подлючения
+/* var_dump(R::testConnection()); // проверка подлючения
+
+$chat_id = 12345;
+$message_id = 54321;
+$message = 'Привет';
+$txt_msg = $message;
+$txt_msq = iconv('UTF-8', 'CP1251', $txt_msg);
 
 
+$user_messages = R::dispense('usermessages');
+$user_messages->chat_id = $chat_id;
+$user_messages->message_id = $message_id;
+$user_messages->message_txt = $txt_msg;
+$id = R::store($user_messages);
 
 
-
+*/
--- global_var.php
+++ global_var.php
@@ -5,11 +5,17 @@ $chat_id = $update['message']['chat']['id'];
 $user_name = $update['message']['from']['username'];
 $message = $update['message']['text'];
 $txt_msg = $message;
-$txt_msq = iconv('UTF-8', 'HTML-ENTITIES', $txt_msg);
+/*$txt_msq = iconv('UTF-8', 'CP1251', $txt_msg);*/
 $message_id = $update['message']['message_id'];
 $message_name = $update['message']['chat']['first_name'];
 
+//Remove UTF8 Bom
 
+function remove_utf8_bom($txt_msq){
+    $bom = pack('H*','EFBBBF');
+    $txt_msq = preg_replace("/^$bom/", '', $txt_msq);
+    return $txt_msq;
+}
 //CREATE to DB RedBeanPHP
 
 $user_messages = R::dispense('usermessages');

b52d119befc65b80468a419e257966528f8cd602
denisveselov <vorle57@gmail.com>
2017-04-30 13:21:26 +0300

    v. 1.1.5.
    callback_res
    
    v. 1.1.6.
    
    подключена DB.

--- global_var.php
+++ global_var.php
@@ -5,22 +5,16 @@ $chat_id = $update['message']['chat']['id'];
 $user_name = $update['message']['from']['username'];
 $message = $update['message']['text'];
 $txt_msg = $message;
-$txt_msq = removeBOM($txt_msq);
+$txt_msq = iconv('UTF-8', 'CP1251', $txt_msg);
 $message_id = $update['message']['message_id'];
 $message_name = $update['message']['chat']['first_name'];
 
-function removeBOM($txt_msq) {
-    if(substr($txt_msq, 0, 3) == pack('CCC', 0xef, 0xbb, 0xbf)) {
-        $txt_msq = substr($txt_msq, 3);
-    }
-    return $txt_msq;
-}
+
 //CREATE to DB RedBeanPHP
 
 $user_messages = R::dispense('usermessages');
 $user_messages->chat_id = $chat_id;
 $user_messages->message_id = $message_id;
-$user_messages->message_txt = $txt_msg;
+/*$user_messages->message_txt = $txt_msg;*/
 $id = R::store($user_messages);
-
 
